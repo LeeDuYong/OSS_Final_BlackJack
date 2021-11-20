0c4d0cbed8cb31fd92aa4979b3453f67b5d90c06
duyong <dy4013@naver.com>
2021-11-21 07:16:19 +0900

    메인 메뉴 페이지 구현

---
+++ mainpage.js
@@ -0,0 +1,16 @@
+let gamestart_button = document.querySelector("#gamestart");
+gamestart_button.addEventListener("click",()=>{
+    window.location.href = "gamepage.html";
+})
+let rules_button = document.querySelector("#rules");
+rules.addEventListener("click",()=>{
+    window.location.href = "rulepage.html";
+})
+let rank_button = document.querySelector("#rank");
+rank_button.addEventListener("click",()=>{
+    window.location.href = "rankpage.html";
+})
+let gameclose_button = document.querySelector("#gameclose");
+gameclose_button.addEventListener("click",()=>{
+    window.location.href = "index.html";
+})
